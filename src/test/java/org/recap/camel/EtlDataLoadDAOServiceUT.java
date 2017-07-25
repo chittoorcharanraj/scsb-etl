6df7ca7429f400804099da4794091a651b56df30
hemalathaselvaraj <hemaselvaraj7@gmail.com>
2017-07-25 17:30:07 +0530

    Added few testcases in etl project

---
+++ src/test/java/org/recap/camel/EtlDataLoadDAOServiceUT.java
@@ -0,0 +1,76 @@
+package org.recap.camel;
+
+import org.junit.Test;
+import org.recap.BaseTestCase;
+import org.recap.model.jpa.BibliographicEntity;
+import org.recap.model.jpa.HoldingsEntity;
+import org.recap.model.jpa.ItemEntity;
+import org.recap.repository.BibliographicDetailsRepository;
+import org.recap.repository.HoldingsDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hemalathas on 25/7/17.
+ */
+public class EtlDataLoadDAOServiceUT extends BaseTestCase{
+
+    @Autowired
+    private EtlDataLoadDAOService etlDataLoadDAOService;
+
+    @Autowired
+    BibliographicDetailsRepository bibliographicDetailsRepository;
+
+    @Autowired
+    HoldingsDetailsRepository holdingsDetailsRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    public void testSavedHoldingsEntity(){
+        Random random = new Random();
+        HoldingsEntity holdingsEntity = etlDataLoadDAOService.savedHoldingsEntity(getHoldingsEntity(random, 1));
+        assertNotNull(holdingsEntity);
+    }
+
+    @Test
+    public void testItemEntity(){
+        ItemEntity itemEntity = new ItemEntity();
+        itemEntity.setCallNumberType("0");
+        itemEntity.setCallNumber("callNum");
+        itemEntity.setCreatedDate(new Date());
+        itemEntity.setCreatedBy("etl");
+        itemEntity.setLastUpdatedDate(new Date());
+        itemEntity.setLastUpdatedBy("etl");
+        itemEntity.setBarcode("33201457127");
+        itemEntity.setOwningInstitutionItemId(".i1231");
+        itemEntity.setOwningInstitutionId(1);
+        itemEntity.setCollectionGroupId(1);
+        itemEntity.setCustomerCode("PA");
+        itemEntity.setItemAvailabilityStatusId(1);
+        ItemEntity savedItemEntity = etlDataLoadDAOService.saveItemEntity(itemEntity);
+        assertNotNull(savedItemEntity);
+    }
+
+    private HoldingsEntity getHoldingsEntity(Random random, Integer institutionId) {
+        HoldingsEntity holdingsEntity = new HoldingsEntity();
+        holdingsEntity.setContent("mock holdings".getBytes());
+        holdingsEntity.setCreatedDate(new Date());
+        holdingsEntity.setCreatedBy("etl");
+        holdingsEntity.setLastUpdatedDate(new Date());
+        holdingsEntity.setLastUpdatedBy("etl");
+        holdingsEntity.setOwningInstitutionId(institutionId);
+        holdingsEntity.setOwningInstitutionHoldingsId(String.valueOf(random.nextInt()));
+        return holdingsEntity;
+    }
+
+}
