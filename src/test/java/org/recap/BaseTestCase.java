4b15cb54a139885801a94eb23ebb24dcac91f19d
DinakarCiber <dnaramala@ciber.com>
2020-07-09 11:59:17 +0530

    changes for fixing failing test cases BasetestCase

--- src/test/java/org/recap/BaseTestCase.java
+++ src/test/java/org/recap/BaseTestCase.java
@@ -6,14 +6,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.*;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Main.class)
+@WebAppConfiguration
 @Transactional
 @Rollback()
 public class BaseTestCase {
