32267c7f019507627e49382e58271ff1b570262b
DinakarCiber <dnaramala@ciber.com>
2020-07-14 23:13:39 +0530

    removing * from BaseTestCase

--- src/test/java/org/recap/BaseTestCase.java
+++ src/test/java/org/recap/BaseTestCase.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
