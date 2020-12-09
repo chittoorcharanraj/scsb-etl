bff8e6f9993c39e50bca833e534d36f48fd1003c
aniitavenkatesan <anitha.venkatesan@htcindia.com>
2020-12-09 10:57:42 +0530

    Junit Changes

--- src/test/java/org/recap/report/CommonReportGeneratorUT.java
+++ src/test/java/org/recap/report/CommonReportGeneratorUT.java
@@ -3,13 +3,17 @@ package org.recap.report;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.apache.camel.component.file.GenericFile;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.support.DefaultExchange;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.recap.BaseTestCase;
+import org.recap.BaseTestCaseUT;
+import org.recap.RecapConstants;
 import org.recap.repositoryrw.ReportDetailRepository;
 import org.recap.util.datadump.DataExportHeaderUtil;
 
@@ -17,7 +21,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CommonReportGeneratorUT extends BaseTestCase {
+public class CommonReportGeneratorUT extends BaseTestCaseUT {
 
     @Mock
     ReportDetailRepository mockReportDetailRepository;
@@ -25,6 +29,7 @@ public class CommonReportGeneratorUT extends BaseTestCase {
     @Mock
     DataExportHeaderUtil dataExportHeaderUtil;
 
+    @InjectMocks
     CommonReportGenerator commonReportGenerator;
 
     @Before
@@ -39,7 +44,10 @@ public class CommonReportGeneratorUT extends BaseTestCase {
         Exchange ex = new DefaultExchange(ctx);
         Message in = ex.getIn();
         ex.setMessage(in);
-        ex.setProperty("INST_NAME", "CUL");
+        ex.setProperty(RecapConstants.INST_NAME, "CUL");
+        GenericFile<Object> camelFileExchangeFile=new GenericFile<>();
+        camelFileExchangeFile.setFileName("file");
+        ex.setProperty(RecapConstants.CAMEL_EXCHANGE_FILE, camelFileExchangeFile);
         Map<String, Object> mapdata = new HashMap<>();
         mapdata.put("institutionName", "CUL");
         in.setHeaders(mapdata);
