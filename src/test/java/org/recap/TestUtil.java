ad3f24218eea9beae57938481a41048801baca8f
aniitavenkatesan <anitha.venkatesan@htcindia.com>
2021-02-08 15:43:22 +0530

    Junit changes

--- src/test/java/org/recap/TestUtil.java
+++ src/test/java/org/recap/TestUtil.java
@@ -1,8 +1,14 @@
 package org.recap;
 
+import org.recap.model.jpa.CollectionGroupEntity;
 import org.recap.model.jpa.ImsLocationEntity;
+import org.recap.model.jpa.InstitutionEntity;
+import org.recap.model.jpa.ItemStatusEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestUtil {
 
@@ -17,5 +23,37 @@ public class TestUtil {
         imsLocationEntity.setImsLocationCode(imsLocationCode);
         imsLocationEntity.setImsLocationName(imsLocationName);
         return imsLocationEntity;
+    }
+
+    public static InstitutionEntity getInstitutionEntity(int id,String institutionCode,String institutionName) {
+        InstitutionEntity institutionEntity=new InstitutionEntity();
+        institutionEntity.setId(id);
+        institutionEntity.setInstitutionCode(String.valueOf(institutionCode));
+        institutionEntity.setInstitutionName(institutionName);
+        return institutionEntity;
+    }
+
+    public static CollectionGroupEntity getCollectionGroupEntities(int id,String collectionGroupCode,String collectionGroupDescription) {
+        CollectionGroupEntity collectionGroupEntity = new CollectionGroupEntity();
+        collectionGroupEntity.setId(id);
+        collectionGroupEntity.setCollectionGroupCode(collectionGroupCode);
+        collectionGroupEntity.setCollectionGroupDescription(collectionGroupDescription);
+        return collectionGroupEntity;
+    }
+
+    public static ItemStatusEntity getItemStatusEntity(int id,String statusCode) {
+        ItemStatusEntity itemStatusEntity = new ItemStatusEntity();
+        itemStatusEntity.setId(id);
+        itemStatusEntity.setStatusCode(statusCode);
+        itemStatusEntity.setStatusDescription(statusCode);
+        return itemStatusEntity;
+    }
+
+    public static List<String> getInstitutionCodeExceptHTC() {
+        List<String> allInstitutionCodeExceptHTC=new ArrayList<>();
+        allInstitutionCodeExceptHTC.add("PUL");
+        allInstitutionCodeExceptHTC.add("CUL");
+        allInstitutionCodeExceptHTC.add("NYPL");
+        return allInstitutionCodeExceptHTC;
     }
 }
