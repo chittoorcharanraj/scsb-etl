f28cc65c3d268f667723337cbfb04ad94952ce52
chittoorcharanraj <chittoor.charanraj@htcinc.com>
2023-04-04 12:51:00 +0530

    Fixed failing test cases for etl

--- src/test/java/org/recap/BaseTestCase.java
+++ src/test/java/org/recap/BaseTestCase.java
@@ -2,27 +2,19 @@ package org.recap;
 
 import org.apache.camel.CamelContext;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 
-import static org.junit.Assert.assertNotNull;
-
 //@ExtendWith(MockitoExtension.class)
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = Main.class)
-@TestPropertySource("classpath:application.properties")
-@WebAppConfiguration
-@Transactional
-@Rollback()
+@RunWith(SpringRunner.class)
+@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Main.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations="classpath:application.properties")
 public class BaseTestCase {
 
     @Autowired
