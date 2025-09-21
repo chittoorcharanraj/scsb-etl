9953e6bdcf0a97cd883e6341eaf548ab5e5f1f8a
chittoorcharanraj <chittoor.charanraj@htcinc.com>
2025-09-21 16:05:53 +0530

    Upgrade activemq version to 6.1.4

--- src/main/java/org/recap/camel/activemq/ActivemqRegistrar.java
+++ src/main/java/org/recap/camel/activemq/ActivemqRegistrar.java
@@ -1,37 +1,38 @@
 package org.recap.camel.activemq;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.camel.component.activemq.ActiveMQComponent;
 import org.apache.camel.CamelContext;
+import org.apache.camel.component.activemq.ActiveMQComponent;
 import org.recap.PropertyKeyConstants;
+import org.recap.ScsbCommonConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSException;
+import org.springframework.stereotype.Component;
 
 
 /**
  * Created by premkb on 12/10/16.
  */
-
 @Component
 public class ActivemqRegistrar {
 
     /**
-     * This class is initialized to register the activemq component to the camel context with the default message broker url.
+     *  Instantiates a new Activemq registrar to use Activemq instead of Camel's in-memory queue.
      *
-     * @param camelContext
-     * @param defaultBrokerURL
-     * @throws JMSException
+     * @param camelContext     the camel context
+     * @param defaultBrokerURL the default broker url
+     * @throws JMSException the jms exception
      */
     @Autowired
-    public ActivemqRegistrar(CamelContext camelContext , @Value("${" + PropertyKeyConstants.ACTIVEMQ_BROKER_URL + "}") String defaultBrokerURL) {
+    public ActivemqRegistrar(CamelContext camelContext , @Value("${" + PropertyKeyConstants.ACTIVEMQ_BROKER_URL + "}") String defaultBrokerURL) throws JMSException {
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(defaultBrokerURL);
         ActiveMQComponent activeMQComponent = new ActiveMQComponent();
         activeMQComponent.setConnectionFactory(connectionFactory);
         activeMQComponent.setTrustAllPackages(true);
-        camelContext.addComponent("scsbactivemq", activeMQComponent);
+        camelContext.addComponent(ScsbCommonConstants.SCSB_ACTIVEMQ_COMPONENT_NAME, activeMQComponent);
     }
+
 }
